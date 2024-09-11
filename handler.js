96f27808dfcd1c1475d3f76c7c8f7045b47072af
Julia De Nadai <denadaijulia@gmail.com>
2024-09-11 19:51:48 -0300

    .

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
-const serverless = require('serverless-http'); // Converte o app Express para Lambda handler
-const { app } = require('./src/api'); // Importa a aplicação Express
+import serverless from 'serverless-http'; // Converte o app Express para Lambda handler
+import app from "./src/api" // Importa a aplicação Express
 
 // Converte a aplicação Express para funcionar no ambiente Lambda
 const handler = serverless(app);
